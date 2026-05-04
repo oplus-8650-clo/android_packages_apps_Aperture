97a58cda277f141562f9e33e69a51e2b86415a46
John Galt <johngaltfirstrun@gmail.com>
2026-05-04 12:24:59 -0400

    Aperture: support oplus super steady key

--- app/src/main/java/org/lineageos/aperture/CameraActivity.kt
+++ app/src/main/java/org/lineageos/aperture/CameraActivity.kt
@@ -94,6 +94,7 @@ import org.lineageos.aperture.ext.setHotPixelMode
 import org.lineageos.aperture.ext.setNoiseReductionMode
 import org.lineageos.aperture.ext.setPadding
 import org.lineageos.aperture.ext.setShadingMode
+import org.lineageos.aperture.ext.setSuperSteadyEnabled
 import org.lineageos.aperture.ext.setVideoStabilizationMode
 import org.lineageos.aperture.ext.slide
 import org.lineageos.aperture.ext.slideDown
@@ -1644,6 +1645,10 @@ open class CameraActivity : AppCompatActivity(R.layout.activity_camera) {
 
                         else -> null
                     } ?: VideoStabilizationMode.OFF
+                )
+                .setSuperSteadyEnabled(
+                    if (cameraConfiguration is CameraConfiguration.Video &&
+                        cameraConfiguration.enableOplusVideoStabilization) true else null
                 )
                 .setEdgeMode(camera2Options.edgeMode)
                 .setNoiseReductionMode(camera2Options.noiseReductionMode)
